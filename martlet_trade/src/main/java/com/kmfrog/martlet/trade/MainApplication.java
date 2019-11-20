e798300c96d3cd6ab925b5d6ecfb96a09db2f8c8
Dust <dust@kmfrog.com>
2019-11-20 21:46:42 +0800

    spring boot(trade)

---
+++ martlet_trade/src/main/java/com/kmfrog/martlet/trade/MainApplication.java
@@ -0,0 +1,15 @@
+package com.kmfrog.martlet.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MainApplication.class);
+        app.run(args);
+
+    }
+
+}
